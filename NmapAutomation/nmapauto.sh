029d3a5cac27cfe6df74c80fc89b0d3f546b3371
Ayaan7345 <ayaan7345@gmail.com>
2024-03-05 08:07:42 -0600

    Added -p- all ports scan to the script

--- NmapAutomation/nmapauto.sh
+++ NmapAutomation/nmapauto.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-#if you move this file into your /bin folder and run a chmod +x nmapauto.sh you can use this script at anytime you want
+# If you move this file into your /bin folder and run a chmod +x nmapauto.sh you can use this script at anytime you want
+
 # Function to run Nmap scan
 run_nmap_scan() {
     local target=$1
+    local output_file="service_and_version_scan_$target.txt"
     echo "Running Nmap scan for target: $target"
-    nmap -sC -sV $target
+    echo "Results will be saved to: $output_file"
+    nmap -sC -sV -p- $target > "$output_file"
+    echo "Nmap scan completed. Results saved to: $output_file"
 }
 
 # Main function
